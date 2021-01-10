57e5b95f9232124b9587541c190f0c1817922959
Badger42S <57646489+Badger42S@users.noreply.github.com>
2021-01-10 11:52:11 -0500

    change main html code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const table = document.getElementById("pole");
+const table = document.getElementById("grid");
 const fasterBtoon = document.getElementById("faster");
 const slowerBtoon = document.getElementById("slower");
 const score = document.getElementById('score');
@@ -12,23 +12,6 @@ let speed = 1000;
 let timer;
 let fastCounter =1;
 
-const randomCel =() =>{ return Math.round(Math.random() * (FIELD_LENGTH)*(FIELD_LENGTH))};
-let foodForSnake =(moveSpeed) =>{
-    clearInterval(timer);
-    timer=setInterval(()=>{
-        step(direction);
-        if (!isFood) {
-            let food =randomCel();
-            while (document.getElementById(food).className === 'select') {
-                food = randomCel();
-            }
-            console.log(food);
-                document.getElementById(food).className='food';  
-            isFood =true;
-        }
-    }, moveSpeed);
-}
-
 let acceleration =()=>{
     if (fastCounter<3) {
         fastCounter++;
@@ -37,31 +20,6 @@ let acceleration =()=>{
         fasterBtoon.click();
     }
 };
-
-let iniPosition =() => {for (let i = 0; i < 3; i++) {
-    let startPosition = document.getElementById(i);
-    startPosition.className = 'select';
-    snake = [2,1,0];   
-    headPosition=snake[0];
-    direction ="ArrowRight";
-    speed = 1000;
-    score.textContent=0;
-    isFood =false;
-    foodForSnake(speed);
-}};
-
-
-for(let i=0; i<19; i++) {
-    let newTr = document.createElement("tr");
-    for (let j = 0; j <19; j++) {
-        let newTd = document.createElement('td');
-        newTd.id = i*19 + j;
-        newTr.appendChild(newTd);
-    }
-    table.appendChild(newTr);
-}
-
-iniPosition();
 
 let step= (way)=> {
     let privState =headPosition;
@@ -102,24 +60,22 @@ let step= (way)=> {
     }
 }
 
-document.addEventListener("keydown", function(event) {
-   switch (event.key) {
-       case 'ArrowUp':
-            direction='ArrowUp';
-           break;
-       case 'ArrowRight':
-           direction='ArrowRight';
-           break;
-       case 'ArrowDown':
-           direction='ArrowDown';
-           break;
-       case 'ArrowLeft':
-           direction='ArrowLeft';
-           break;
-       default:
-           break;
-   };
-});
+const randomCel =() =>{ return Math.round(Math.random() * (FIELD_LENGTH)*(FIELD_LENGTH))};
+let foodForSnake =(moveSpeed) =>{
+    clearInterval(timer);
+    timer=setInterval(()=>{
+        step(direction);
+        if (!isFood) {
+            let food =randomCel();
+            while (document.getElementById(food).className === 'select') {
+                food = randomCel();
+            }
+            console.log(food);
+                document.getElementById(food).className='food';  
+            isFood =true;
+        }
+    }, moveSpeed);
+}
 
 fasterBtoon.addEventListener('click', ()=>{
     if (speed>100) {
@@ -134,3 +90,42 @@ slowerBtoon.addEventListener('click', ()=>{
         foodForSnake(speed);
     }
 })
+
+let iniPosition =() => {for (let i = 0; i < 3; i++) {
+    let startPosition = document.getElementById(i);
+    startPosition.className = 'select';
+    snake = [2,1,0];   
+    headPosition=snake[0];
+    direction ="ArrowRight";
+    speed = 1000;
+    score.textContent=0;
+    isFood =false;
+    foodForSnake(speed);
+}};
+
+for(let i=0; i<361; i++) {
+    let newCell = document.createElement('section');
+    newCell.id = i;
+    table.appendChild(newCell);
+}
+
+document.addEventListener("keydown", function(event) {
+    switch (event.key) {
+        case 'ArrowUp':
+             direction='ArrowUp';
+            break;
+        case 'ArrowRight':
+            direction='ArrowRight';
+            break;
+        case 'ArrowDown':
+            direction='ArrowDown';
+            break;
+        case 'ArrowLeft':
+            direction='ArrowLeft';
+            break;
+        default:
+            break;
+    };
+ });
+
+iniPosition();
